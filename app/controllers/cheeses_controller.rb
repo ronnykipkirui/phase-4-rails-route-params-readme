5d2a2b45fed23cea849334581e1038d797795273
Ronny <ronnybet41@gmail.com>
2022-11-22 15:40:14 +0300

    completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,13 @@
+
+
 class CheesesController < ApplicationController
 
   def index
     cheeses = Cheese.all
     render json: cheeses
+  end
+  def show
+    render json: Cheese.find(params[:id])
   end
 
 end
